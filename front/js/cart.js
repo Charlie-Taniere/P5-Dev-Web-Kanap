f9917f00aaef20054130ffbff17837bad37e84d4
charlie.taniere <charlie.taniere@gmail.com>
2022-02-17 15:42:21 +0100

    cart render array fix

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,36 +1,38 @@
+// Récupération du localstorage
+
 let retrieveBasket = localStorage.getItem("productInformation");
 let basketProduct;
 let product;
 let price;
+let addQuantity = 0
 
+// Fonction pour afficher le panier 
 async function renderCart() {
 
     basketProduct = JSON.parse(retrieveBasket);
     console.log(basketProduct);
 
-
+    // Boucle qui itère l'API     
+    for (let i = 0; i < basketProduct.length; i++) {
+        productId = basketProduct[i];
+        url = "http://localhost:3000/api/products/" + productId.id;
+        await fetch(url)
+            .then(function (res) {
+                if (res.ok) {
+                    return res.json().then(data => {
+                        price = data.price;
+                        console.log(price);
+                    });
+                }
+            })
+            .catch(function (err) {
+                console.log("Une erreur est survenue")
+            })
+    }
+    let html = " ";
+// Boucle pour chaque produit dans le localstorage
     for (product of basketProduct) {
 
-        // for (var i = 0; i < basketProduct.length; i++) {
-        //     product = basketProduct[i];
-        //     url = "http://localhost:3000/api/products/" + product.id;
-        //     await fetch(url)
-        //         .then(function (res) {
-        //             if (res.ok) {
-        //                 return res.json().then(data => {
-        //                     price = data.price;
-        //                     console.log(price);
-        //                 });
-        //             }
-        //         })
-        //         .catch(function (err) {
-        //             console.log("Une erreur est survenue")
-        //         })
-        // }
-
-
-
-        let html = " ";
         let htmlSegment = `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
                                 <div class="cart__item__img">
                                     <img src="${product.image}" alt="${product.alt}">
@@ -39,12 +41,12 @@ async function renderCart() {
                                     <div class="cart__item__content__description">
                                     <h2>${product.name}</h2>
                                     <p>${product.color}</p>
-                                    <p>${price}€</p>
+                                    <p>${price * product.quantity}€</p>
                                     </div>
                                     <div class="cart__item__content__settings">
                                     <div class="cart__item__content__settings__quantity">
                                         <p>Qté :${product.quantity} </p>
-                                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${addQuantity}">
                                     </div>
                                     <div class="cart__item__content__settings__delete">
                                         <p class="deleteItem">Supprimer</p>
@@ -52,6 +54,7 @@ async function renderCart() {
                                     </div>
                                 </div>
                                 </article>`;
+                                
         html += htmlSegment;
         let container = document.querySelector("#cart__items");
         container.innerHTML = html;
@@ -59,3 +62,15 @@ async function renderCart() {
 }
 
 renderCart()
+
+window.onload = function () {
+document.getElementsByName("itemQuantity")[0].addEventListener("change", doThing);
+function doThing(){
+    alert('Horray! Someone wrote "' + this.value + '"!');
+}
+}
+
+let itemQuantity = document.getElementsByName("itemQuantity");
+document.body.addEventListener("change", function (e) {
+let 
+});
